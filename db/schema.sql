54ae202b5ddfcf5cceaf99c410bef02a80833a2c
unknown <dipisha03@gmail.com>
2018-02-27 21:59:57 -0500

    new push

--- db/schema.sql
+++ db/schema.sql
@@ -5,32 +5,17 @@ USE alchemy_db;
 
 CREATE TABLE alchemy_table (
 	id int NOT NULL AUTO_INCREMENT,
-	username varchar(200) NULL,
-    gender varchar (300) NULL, 
+	username varchar(200) NOT NULL,
+    gender varchar (300) NOT NULL, 
     age INT (500) NOT NULL,
-	sign varchar (400) NULL,
-    photo url (500) NULL,
+	sign varchar (400) NOT NULL,
+    photo url (500) NOT NULL,
 	PRIMARY KEY (id)
 );
 
 CREATE TABLE alchemy_match (
 	id int NOT NULL AUTO_INCREMENT,
-	sign varchar(500) NULL,
-	match1 varchar (500) NULL,
-	match2 varchar (500) NULL,
-	match3 varchar (500) NULL,
-	match4 varchar(500) NULL,
-	match5 varchar(500) NULL,
-	match6 varchar(500) NULL,
-	match7 varchar(500) NULL,
-	match8 varchar(500) NULL,
-	match9 varchar(500) NULL,
-	match10 varchar(500) NULL,
-	match11 varchar(500) NULL,
-	match12 varchar(500) NULL,
+	sign varchar(500) NOT NULL,
+	possibleMatch varchar (500) NOT NULL,
 	PRIMARY KEY (id)
 );
-
-INSERT (sign, match1, match2) VALUES ('Virgo', 'Libra', 'Capricorn')
-INSERT (sigg, match2, match3, match4) VALUES ('Cancer', 'Libra', 'Virgo', 'pisces')
-
--- db/seeds.sql
+++ db/seeds.sql
@@ -3,48 +3,24 @@ INSERT INTO alchemy_table (username, gender, age, sign, photo);
 
 SELECT * FROM alchemy_table;
 
-INSERT INTO alchemy_match (match1);
-INSERT INTO alchemy_match (match2);
-INSERT INTO alchemy_match (match3);
-INSERT INTO alchemy_match (match4);
-INSERT INTO alchemy_match (match5);
-INSERT INTO alchemy_match (match6);
-INSERT INTO alchemy_match (match7);
-INSERT INTO alchemy_match (match8);
-INSERT INTO alchemy_match (match9);
-INSERT INTO alchemy_match (match10);
-INSERT INTO alchemy_match (match11);
-INSERT INTO alchemy_match (match12);
-
-SELECT * FROM alchemy_table;
-
 INSERT INTO alchemy_match 
 SELECT id 
 FROM alchemy_table
-WHERE sign=aries AND sign=aries AND
-WHERE sign=aries AND sign=aries AND 
-WHERE sign=aries AND sign=aries AND 
-WHERE sign=aries AND sign=aries AND 
-WHERE sign=aries AND sign=aries AND 
-WHERE sign=aries AND sign=aries AND 
-WHERE sign=aries AND sign=aries AND 
-WHERE sign=aries AND sign=aries AND 
-
-SELECT
-  id
-FROM
-  alchemy_table 
-WHERE 
-  (sign = 'aries') OR (sign = 'leo') OR (sign = 'sagittarius') 
-  OR (sign = 'taurus') OR (sign = 'virgo') OR (sign = 'capricorn') 
-  OR (sign = 'gemini') OR (sign = 'libra') OR (sign = 'aquarius') 
-  OR (sign = 'cancer') OR (sign = 'scorpio') OR (sign = 'pisces')
+WHERE sign=aries AND possibleMatch in{"aries", "leo", "sagittarius", "gemini", "libra", "aquarius"},
+WHERE sign=leo AND possibleMatch in{"aries", "leo", "sagittarius", "gemini", "libra"},
+WHERE sign=sagittarius AND possibleMatch in{"aries", "leo", "sagittarius", "gemini", "libra", "aquarius"},
+WHERE sign=taurus AND possibleMatch in{"taurus", "virgo", "capricorn", "cancer", "scorpio", "pieces"},
+WHERE sign=virgo AND possibleMatch in{"taurus", "virgo", "capricorn", "cancer", "scorpio"},
+WHERE sign=capricorn AND possibleMatch in{"taurus", "virgo", "capricorn", "cancer", "scorpio", "pieces"},
+WHERE sign=gemini AND possibleMatch in{"aries", "leo", "gemini", "libra", "aquarius"},
+WHERE sign=libra AND possibleMatch in{"leo", "sagittarius", "gemini", "libra", "aquarius"},
+WHERE sign=aquarius AND possibleMatch in{"aries", "leo", "sagittarius", "gemini", "libra", "aquarius"},
+WHERE sign=cancer AND possibleMatch in{"taurus", "virgo", "capricorn", "cancer", "scorpio", "pieces"},
+WHERE sign=scorpio AND possibleMatch in{"taurus", "virgo", "capricorn", "cancer", "scorpio", "pieces"},
+WHERE sign=pisces AND possibleMatch in{"taurus", "capricorn", "cancer", "scorpio", "pieces"};
 
 // pulls out all matching signs based on user selection
 var possiblesMatch = SELECT * FROM alchemy_match WHERE name='user-input'
 
 // uses matching signs to find users 
-SELECT  * FROM alchemy_Table WHERE sign = possibleMatch
-
-
-ID NAME MA
+SELECT * FROM alchemy_Table WHERE sign = possibleMatch
