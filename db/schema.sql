62fc58cbf5dd229bf50d903c698b18402a7d82f6
unknown <dipisha03@gmail.com>
2018-02-28 19:13:04 -0500

    updated

--- db/schema.sql
+++ db/schema.sql
@@ -6,10 +6,10 @@ USE alchemy_db;
 CREATE TABLE alchemy_table (
 	id int NOT NULL AUTO_INCREMENT,
 	username varchar(200) NOT NULL,
-    gender varchar (300) NOT NULL, 
-    age INT (500) NOT NULL,
+	gender varchar (300) NOT NULL,
+	age INT (500) NOT NULL,
 	sign varchar (400) NOT NULL,
-    photo url (500) NOT NULL,
+	photo url (500) NOT NULL,
 	possibleMatch varchar (500) NOT NULL,
 	PRIMARY KEY (id)
 );
